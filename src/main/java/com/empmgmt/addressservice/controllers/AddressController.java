6dade953831d86860d7fe6df72939982ff5987ec
Abhishek Sable <abhisheksable@Abhisheks-MacBook-Air.local>
2024-02-05 21:55:08 +0530

    changed response

--- src/main/java/com/empmgmt/addressservice/controllers/AddressController.java
+++ src/main/java/com/empmgmt/addressservice/controllers/AddressController.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class AddressController {
 	@GetMapping("/address")
 	public String getAddress() {
-		return "My address is semriya Pandri Tehsil Multai Madhya Pradesh";
+		return "My address is semriya Pandri Tehsil Multai Madhya Pradesh Betul";
 
 	}
 }
