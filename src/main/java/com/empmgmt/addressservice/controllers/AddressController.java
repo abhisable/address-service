c397edb27c3ef643ae18cc8cc5b3c392193118c4
Abhishek Sable <abhisheksable@Abhisheks-MacBook-Air.local>
2024-02-05 21:27:29 +0530

    first commit

---
+++ src/main/java/com/empmgmt/addressservice/controllers/AddressController.java
@@ -0,0 +1,13 @@
+package com.empmgmt.addressservice.controllers;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class AddressController {
+	@GetMapping("/address")
+	public String getAddress() {
+		return "My address is semriya Pandri Tehsil Multai Madhya Pradesh";
+
+	}
+}
